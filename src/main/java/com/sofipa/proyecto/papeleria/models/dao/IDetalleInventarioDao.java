ac6d65883358812fa9f1d85d3d888cc9a9bf9026
ChrisD1J9A <diego-19gyc@hotmail.com>
2022-04-07 16:50:48 -0500

    07/04/2022

--- src/main/java/com/sofipa/proyecto/papeleria/models/dao/IDetalleInventarioDao.java
+++ src/main/java/com/sofipa/proyecto/papeleria/models/dao/IDetalleInventarioDao.java
@@ -12,6 +12,15 @@ public interface IDetalleInventarioDao extends CrudRepository<detalle_inventario
 			+ "WHERE di.id_inventario= :id", nativeQuery = true)
 	List<detalle_inventario> detalles_de_iventario (@Param("id") long id);  
 
-	@Query(value= "SELECT i.nombre_sucursal, i.id_inventario, di.id_producto, di.cant_existente FROM inventario i, detalle_inventario di ORDER BY i.id_sucursal ASC;", nativeQuery = true)
+	@Query(value= "SELECT\r\n"
+			+ "	i.id_inventario as 'Id_inventario',\r\n"
+			+ "	i.nombre_sucursal as 'Sucursal',\r\n"
+			+ "	p.descripcion as 'Productos',\r\n"
+			+ "	di.cant_existente as 'Cantidad existente'\r\n"
+			+ "FROM inventario i, detalle_inventario di\r\n"
+			+ "JOIN productos p\r\n"
+			+ "ON   di.id_producto = p.id_producto\r\n"
+			+ "WHERE i.id_inventario = di.id_inventario\r\n"
+			+ "ORDER BY i.id_sucursal ASC", nativeQuery = true)
 	List<Object> todos_los_inventarios_ordenados ();
 }
